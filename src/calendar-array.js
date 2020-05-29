c5997eec876114fba5cd8754101bb17151df0583
itmor <itmor.developer@gmail.com>
2020-05-29 15:00:37 +0300

    add getMonthDays()

--- src/calendar-array.js
+++ src/calendar-array.js
@@ -1,10 +1,35 @@
 class CalendarArray {
   getCalendar(...dates) {
     if (this.#isValidDateArg(dates)) {
-      console.log('is valid');
+      this.#createCalendarArray(dates);
     }
   }
 
+  #createCalendarArray(dates) {
+    for (let date of dates) {
+
+      for (let i = 0; i < date.length; i++) {
+        if (i === 0) {
+          const year = date[0];
+        } else {
+
+        }
+      }
+    }
+  }
+
+  #getMonthDays(year, month) {
+    // Correct date 2020, 12 | Wrong date 99999, 99
+    if (String(year).length < 5 && String(month).match(/^([1-9]|1[0-2])$/) !== null) {
+      const date = new Date(year, month - 1);
+      const daysValue = new Date(date.getFullYear(), date.getMonth()+1, 0);
+
+      return daysValue.getDate();
+    } 
+
+    throw new Error('Wrong date');
+  }
+ 
   #isValidDateArg(dates) {
     // Sample correct arguments [[2020, 3], [2012], [2007]]
     if (dates.length > 0) {
